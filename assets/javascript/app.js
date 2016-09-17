a8ed165423915cf72fcf0b61416f0fbef75e740f
Michael Constanza <michael.constanza@gmail.com>
2016-09-17 18:02:30 -0400

    working skeleton of database. Two players can be set up from different clients without overlapping and delete will be properly deleted from database if user disconnects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,89 @@
 
  var database = firebase.database();
 
+ var players;
+ var player1Name;
+ var player2Name;
+
+// Database Snapshots ////////////////////////////////////////////
+
+// Player 1 Name Changes
+database.ref('/players/1/name').on('value', function(snapshot){
+
+    player1Name = snapshot.val();
+    console.log(player1Name);
+})
+
+// Player 2 Name Changes
+database.ref('/players/2/name').on('value', function(snapshot){
+
+    player2Name = snapshot.val();
+    console.log(player2Name);
+})
+
+ // Game Variables ///////////////////////////////////////////////////
+ var gameStarted = false;
+
+// Functions /////////////
+
+function playGame(){
+    // On Player Disconnect
+    console.log('database ref child: ' + database.ref('/players').child(userID))
+    database.ref('/players').child(userID).onDisconnect().remove()
+}
+
 // jQuery ////////////////////////////////////////////////////////////
  $(document).ready(function(){
+     // jQuery Elements //////////////////////////////////////////////
+     $playButton = $('#playButton');
+     $nameInput = $('#nameInput');
+     $addPlayerForm = $('#addPlayerForm');
 
+
+     // Play Game Button
+     $playButton.on('click', function(){
+
+        // set name to value in field
+        var name = $nameInput.val().trim();
+
+        // if player1 and 2 both exist, do nothing
+        if (player1Name && player2Name){
+            return false;
+
+        // if player1 exists, add player 2
+        }else if (player1Name){
+            console.log('adding player 2')
+            userID = 2;
+            console.log('user: ' + userID)
+            database.ref('/players/2').update({
+                name: name,
+                user: userID,
+                wins: 0,
+                losses: 0
+            });
+
+            // disable add player form and button once players have been selected
+            // $addPlayerForm.hide();
+            // $playButton.hide();
+
+        // if neither player exists, add player 1
+        }else {
+            console.log('adding player 1')
+            userID = 1;
+            console.log('user: ' + userID)
+            database.ref('/players/1').update({
+                name: name,
+                user: userID,
+                wins: 0,
+                losses: 0
+            })
+        }
+        // reset form field
+        $nameInput.val("");
+
+        playGame();
+        // prevent page refresh
+        return false;
+    });
 
  }); // end of jQuery
