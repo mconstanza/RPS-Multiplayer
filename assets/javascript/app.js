f61cb5d5690d789fc16f46448480295d1aac30d2
Michael Constanza <michael.constanza@gmail.com>
2016-09-17 12:20:32 -0400

    initial commit.

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// RPS - Multiplayer
+// Rock, Paper, Scissors made using Javascript and firebase
+
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDTDDqqoNnWuCDCf6aQGpo_1UF5NqWE9oA",
+   authDomain: "rps-multiplayer-6b3c9.firebaseapp.com",
+   databaseURL: "https://rps-multiplayer-6b3c9.firebaseio.com",
+   storageBucket: "rps-multiplayer-6b3c9.appspot.com",
+   messagingSenderId: "944278702942"
+ };
+ firebase.initializeApp(config);
+
+ var database = firebase.database();
+
+// jQuery ////////////////////////////////////////////////////////////
+ $(document).ready(function(){
+
+
+ }); // end of jQuery
